6e46d94e3eaa079cf13ef38c1eada88cc7079904
Gabriel Albuquerque <albuquerque.se@proton.me>
2024-01-13 00:43:33 -0300

    feat: adding some buffeline confs

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,3 +1,9 @@
 -- Bufferline confs
 
-require("bufferline").setup{}
+require("bufferline").setup{
+    options = {
+        always_show_bufferline = false,
+        show_close_icon = false,
+        color_icons = true
+    }
+}
